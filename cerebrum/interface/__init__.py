c903a179512b3de9b16b20ba6d3cd6396acac18f
BRama10 <grand.hunter.dark.15@gmail.com>
2024-10-08 22:02:21 -0400

    feat: exposed interface classes

--- cerebrum/interface/__init__.py
+++ cerebrum/interface/__init__.py
@@ -1,4 +1,36 @@
-
+from cerebrum.manager.manager import AgentManager
+from cerebrum.llm.adapter import LLMAdapter
 
 class AutoAgent:
-    pass
+    MANAGER = AgentManager('https://my.aios.foundation')
+
+    @classmethod
+    def from_pretrained(cls, name: str):
+        _author, _name = name.split('/')
+
+        _author, _name, _version = cls.MANAGER.download_agent(_author, _name)
+
+        agent = cls.MANAGER.load_agent(
+            _author,
+            _name,
+            _version
+        )
+
+        return agent
+
+
+class AutoLLM:
+
+    @classmethod
+    def from_foundational(cls, name: str):
+        _llm_factory = LLMAdapter(name)
+
+        _llm = _llm_factory.get_model()
+
+        return _llm
+
+
+class AutoAgentGenerator:
+    
+    @classmethod
+    build_agent()
